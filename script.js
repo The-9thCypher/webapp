61eafcaa692326c7138814dbf4889717e4ed1d54
Shyllon Olayinka <shyllonolayinka608@gmail.com>
2022-10-13 17:09:07 +0100

    buttons

---
+++ script.js
@@ -0,0 +1,12 @@
+function message() {
+    window.open('https://api.whatsapp.com/send?phone=2348179726172&text=The9thCypher' , '_blank');
+}
+
+function github() {
+    window.open('https://github.com/The-9thCypher' , '_blank');
+}
+
+function linkedin() {
+    window.open('https://www.linkedin.com/in/shyllon-olayinka-31b925247/' , '_blank');
+}
+
